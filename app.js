239eefd7421bca2d8a805823339332397958ea4e
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-27 23:17:40 -0300

    Pegos elementos do HTML e criada constante com URL da API

---
+++ app.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('form');
+const searchInput = document.getElementById('search');
+const songsContainer = document.getElementById('songs-container');
+const prevAndNextContainer = document.getElementById('prev-and-next-container');
+
+const apiURL = 'https://api.lyrics.ovh'
